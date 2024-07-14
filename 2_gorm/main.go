bf1b1c38b0ba31f54b4e514db8f4e265be88be47
bdmoriki <dupim1@hotmail.com>
2024-07-14 20:54:56 -0300

    Adicionando aula de consulta usando o GORM

--- 2_gorm/main.go
+++ 2_gorm/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -19,15 +21,29 @@ func main() {
 	}
 	db.AutoMigrate(&Produto{})
 
-	db.Create(&Produto{
-		Nome:  "Microfone HyperX",
-		Preco: 500.00})
+	// db.Create(&Produto{
+	// 	Nome:  "Microfone HyperX",
+	// 	Preco: 500.00})
 
 	// create batch
-	produtos := []Produto{
-		{Nome: "Teclado", Preco: 200.50},
-		{Nome: "Monitor", Preco: 3000.00},
-		{Nome: "Mouse", Preco: 150.50}}
+	// produtos := []Produto{
+	// 	{Nome: "Teclado", Preco: 200.50},
+	// 	{Nome: "Monitor", Preco: 3000.00},
+	// 	{Nome: "Mouse", Preco: 150.50}}
 
-	db.Create(produtos)
+	// db.Create(produtos)
+
+	// var produto Produto
+	// db.First(&produto, 1)
+	// fmt.Println(produto)
+
+	// db.First(&produto, "nome = ?", "Teclado")
+	// fmt.Println(produto)
+
+	var produtos []Produto
+	db.Find(&produtos)
+
+	for _, p := range produtos {
+		fmt.Println(p)
+	}
 }
